9d1bbf1c133b1cb875fac058ea81fe1de6c28187
Syed Akif Hussain Zaidi <116806308+AkifZaidi@users.noreply.github.com>
2022-11-03 04:42:21 +0500

    I do practice js chapter 9 to 14

---
+++ app.js
@@ -0,0 +1,66 @@
+// chapter: 9 practice.
+// PROMPT
+var main = prompt("Enter a name");
+console.log(main)
+// chapter: 9 complete.
+
+// chapter: 10 practice.
+// IF STATEMENTS
+ var x = prompt("Where does the Pope live?");
+ if (x === "Vatican") {
+ alert("Correct!");
+ }
+// chapter: 10 complete.
+
+// chapter: 11 practice.
+
+// COMPERISON OPERATORS
+
+//> is greater than
+//< is less than
+//>= is greater than or equal to
+//<= is less than or equal to
+
+// chapter: 11 complete.
+
+ // chapter: 12 practice.
+ // IF ELSE AND ELSE IF STATEMENTS
+var user_name = prompt("officer");
+if(user_name === "true"){
+    console.log("work is submited")
+}else if( user_name === "false"){
+    console.log("work is not submited")
+}else{
+    console.log("wapsi ghar" )
+}
+ // chapter: 12 complete.
+
+ // chapter: 13 practice.
+ // TESTING SETS OF CONDITIONS
+ var weight = prompt("Enter a Weigth")
+ var time = prompt("Enter a time")
+ var age = prompt("Enter a age")
+ var gender = prompt("Enter a gendermale")
+
+ if(weight > 300 || time < 6 || age > 17 || gender === "male"){
+    console.log("Come to our tryout!")
+ }else{
+     console.log("Come to our cookout!")
+ }
+ // chapter: 13 complete.
+
+ // chapter: 14 practice.
+ // IF STATMENTS NESTED
+ var men = prompt("Enter a gender")
+ var women = prompt("Enter a female")
+if(men === "akif"){
+    console.log("boy")
+    if(women === "sadaf"){
+        console.log("girl")
+    }else{
+        console.log("she is not a girl")
+    }
+}else{
+    console.log("he is not a boy name")
+}
+ // chapter: 14 complete.
